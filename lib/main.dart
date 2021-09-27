9c0bc5a975a755afcdf21771bf898292da4fbd49
BulashAS <bulash.lex@gmail.com>
2021-09-27 15:17:39 +0700

    4. Первичная инициализация

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:game_my_copy/game_core/main_loop.dart';
 
 void main() {
-  
-  
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Game"),
+      ),
+    ));
+  });
 }
